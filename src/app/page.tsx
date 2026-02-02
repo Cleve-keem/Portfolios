7ea94d5216871b1574d89a0891bf5fa3ea34f034
Cleve-keem <belloharyourmidey@gmail.com>
2026-02-02 15:31:27 -0600

    chore: add open navbar icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import { TextAlignEnd } from "lucide-react";
 export default function page() {
   return (
     <div>
-      <header className="sticky top-0 p-4 shadow-md">
-        <div>
-          <span className="inline-flex justify-center bg-brand text-white rounded size-6">
+      <header className="sticky top-0 p-4 shadow-md flex">
+        <div className="flex-1">
+          <span className="inline-flex justify-center bg-brand text-white rounded size-6 font-semibold">
             H
           </span>
           <small>ackhim</small>
