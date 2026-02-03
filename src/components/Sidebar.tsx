bc671688e597d131b260921f0c6c1d84192be0f8
Cleve-keem <belloharyourmidey@gmail.com>
2026-02-03 08:29:00 -0600

    fix: side transition out

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -11,8 +11,8 @@ type SidebarProps = {
 export default function Sidebar({ open, closeFn }: SidebarProps) {
   return (
     <aside
-      className={`fixed inset-0 z-100 bg-background w-full h-dvh duration-700 ease-out transition-all transform-gpu ${open ? "translate-x-0 translate-y-0 opacity-100 [clip-path:polygon(0_0,100%_0,100%_100%,0%_100%)]" : "-translate-x-50 -translate-y-50 opacity-0 [clip-path:polygon(0_0,0_0,0_0)]"}`}
-      // className={`fixed top-0 left-0 bg-background w-full duration-500 ease-out ${open ? "translate-0 h-full" : "-translate-full h-0"}`}
+      // className={`fixed inset-0 z-100 bg-background w-full h-dvh duration-700 transition-all ${open ? "translate-x-0 translate-y-0 opacity-100 [clip-path:polygon(0_0,100%_0,100%_100%,0%_100%)]" : "-translate-x-50 -translate-y-50 opacity-0 [clip-path:polygon(0_0,0_0,0_0)]"}`}
+      className={`fixed inset-0 bg-background w-full translate-all duration-500 ease-out ${open ? "translate-x-0 translate-y-0 h-full" : "-translate-x-full -translate-y-full h-0"}`}
     >
       <div className="sidebar-header flex border-b border-b-surface-muted p-6">
         <Logo />
