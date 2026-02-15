57249b2c27076811a2e3804071db491f9fecfde6
Cleve-keem <belloharyourmidey@gmail.com>
2026-02-14 19:36:03 -0600

    fix: resolved params component error

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,5 +1,25 @@
-type Props = { params: { slug: string } };
+// "use client";
 
-export default function Page({ params }: Props) {
-  return <div>Project: {params.slug}</div>;
+// import { use } from "react";
+
+// type Props = {
+//   params: Promise<{ slug: string }>;
+// };
+
+// export default async function Page({ params }: Props) {
+//   const resolvedParams = use(params);
+//   console.log(resolvedParams)
+
+//   return <div>Project:</div>;
+// }
+
+// app/project/[slug]/page.tsx (Server Component)
+
+type Props = {
+  params: Promise<{ slug: string }>;
+};
+
+export default async function Page({ params }: Props) {
+  const { slug } = await params; // Simple await on the server
+  return <div>Project: {slug}</div>;
 }
