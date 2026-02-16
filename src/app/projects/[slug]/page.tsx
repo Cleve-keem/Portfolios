87fe7553f450214a8991467cdec8230e7b306203
Cleve-keem <belloharyourmidey@gmail.com>
2026-02-16 13:52:36 -0600

    feat: added back to previous page functionality

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,7 +1,9 @@
+import Footer from "@/components/Footer";
 import SlideInViewReveal from "@/components/SlideInViewReveal";
 import { projectData } from "@/constants/projects";
 import { ArrowLeft } from "lucide-react";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 type Props = {
   params: Promise<{ slug: string }>;
@@ -10,49 +12,53 @@ type Props = {
 export default async function Page({ params }: Props) {
   const { slug } = await params;
   const project = projectData.find((project) => project.slug === slug);
+  const router = useRouter();
 
   return (
-    <div className="p-8">
-      <ArrowLeft className="mb-7" />
-      <SlideInViewReveal className="relative w-full min-h-100 rounded-2xl overflow-hidden mb-10">
-        <Image
-          src={project?.coverImage as string}
-          alt={project?.name as string}
-          fill
-          priority
-          className="object-cover"
-        />
-      </SlideInViewReveal>
-      <h1 className="text-3xl mb-4">{project?.name}</h1>
-      <p className="mb-4 leading-7">{project?.longDescription}</p>
-      <SlideInViewReveal className="mb-5">
-        <h3 className="mb-4 text-2xl">Stacks</h3>
-        <ul className="space-y-3 px-5">
-          {project?.stack.map((s, i) => (
-            <li key={i}>{s}</li>
-          ))}
-        </ul>
-      </SlideInViewReveal>
-      <SlideInViewReveal>
-        <h3 className="mb-4 text-2xl">Features</h3>
-        <ol className="space-y-3 px-5 mb-10">
-          {project?.features.map((feature, i) => (
-            <li key={i}>{feature}</li>
-          ))}
-        </ol>
-      </SlideInViewReveal>
-      <SlideInViewReveal className="mb-5">
-        <h3 className="text-2xl mb-5">Challenges</h3>
-        <p className="px-5 leading-7">{project?.challenges}</p>
-      </SlideInViewReveal>
-      <SlideInViewReveal className="mb-5">
-        <h3 className="text-2xl mb-4">Solutions</h3>
-        <p className="px-5 leading-7">{project?.solutions}</p>
-      </SlideInViewReveal>
-      <SlideInViewReveal>
-        <h3 className="text-2xl mb-5">Lessons</h3>
-        <p className="px-5 leading-7">{project?.lessons}</p>
-      </SlideInViewReveal>
-    </div>
+    <>
+      <div className="p-8">
+        <ArrowLeft className="mb-7" onClick={() => router.back()} />
+        <SlideInViewReveal className="relative w-full min-h-100 rounded-2xl overflow-hidden mb-10">
+          <Image
+            src={project?.coverImage as string}
+            alt={project?.name as string}
+            fill
+            priority
+            className="object-cover"
+          />
+        </SlideInViewReveal>
+        <h1 className="text-3xl mb-4">{project?.name}</h1>
+        <p className="mb-4 leading-7">{project?.longDescription}</p>
+        <SlideInViewReveal className="mb-5">
+          <h3 className="mb-4 text-2xl">Stacks</h3>
+          <ul className="space-y-3 px-5">
+            {project?.stack.map((s, i) => (
+              <li key={i}>{s}</li>
+            ))}
+          </ul>
+        </SlideInViewReveal>
+        <SlideInViewReveal>
+          <h3 className="mb-4 text-2xl">Features</h3>
+          <ol className="space-y-3 px-5 mb-10">
+            {project?.features.map((feature, i) => (
+              <li key={i}>{feature}</li>
+            ))}
+          </ol>
+        </SlideInViewReveal>
+        <SlideInViewReveal className="mb-5">
+          <h3 className="text-2xl mb-5">Challenges</h3>
+          <p className="px-5 leading-7">{project?.challenges}</p>
+        </SlideInViewReveal>
+        <SlideInViewReveal className="mb-5">
+          <h3 className="text-2xl mb-4">Solutions</h3>
+          <p className="px-5 leading-7">{project?.solutions}</p>
+        </SlideInViewReveal>
+        <SlideInViewReveal>
+          <h3 className="text-2xl mb-5">Lessons</h3>
+          <p className="px-5 leading-7">{project?.lessons}</p>
+        </SlideInViewReveal>
+      </div>
+      <Footer />
+    </>
   );
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import { motion } from "framer-motion";
-import { GithubIcon, Twitter, Facebook, ArrowUpRight } from "lucide-react";
+import { GithubIcon, Twitter, ArrowUpRight, LinkedinIcon } from "lucide-react";
 
 export default function Footer() {
   const currentYear = new Date().getFullYear();
@@ -11,7 +11,6 @@ export default function Footer() {
     <footer className="relative px-6 py-12 md:py-20 border-t border-border-muted bg-canvas overflow-hidden">
       {/* Subtle Background Glow for Depth */}
       <div className="absolute top-0 left-1/2 -translate-x-1/2 w-1/2 h-px bg-linear-to-r from-transparent via-brand to-transparent opacity-50" />
-
       <div className="max-w-6xl mx-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-12 mb-16">
           {/* LEFT: Branding & Message */}
@@ -34,9 +33,17 @@ export default function Footer() {
             <div className="flex items-center gap-4">
               {/* Social Icons with Guru Hover */}
               {[
-                { icon: <GithubIcon size={18} />, href: "#", label: "Github" },
+                {
+                  icon: <GithubIcon size={18} />,
+                  href: "https:github.com/Cleve-keem",
+                  label: "Github",
+                },
                 { icon: <Twitter size={18} />, href: "#", label: "Twitter" },
-                { icon: <Facebook size={18} />, href: "#", label: "Facebook" },
+                {
+                  icon: <LinkedinIcon size={18} />,
+                  href: "#",
+                  label: "Facebook",
+                },
               ].map((social, i) => (
                 <Link
                   key={i}
@@ -64,7 +71,7 @@ export default function Footer() {
               </p>
 
               <Link
-                href="mailto:hello@example.com"
+                href="mailto:hakeembello983@gmail.com"
                 className="inline-flex items-center justify-center w-full gap-2 px-6 py-4 bg-brand text-white rounded-2xl font-bold text-sm group-hover:shadow-lg group-hover:shadow-brand/20 transition-all duration-300"
               >
                 Get in touch
@@ -79,18 +86,9 @@ export default function Footer() {
 
         {/* BOTTOM: Legal & Copyright */}
         <div className="pt-8 border-t border-border-muted flex flex-col md:flex-row justify-between gap-4">
-          <p className="text-[11px] uppercase tracking-widest text-content-muted font-bold">
-            © {currentYear} Hackhim — Crafted with pro touch 😂
+          <p className="text-center text-[11px] capitalize tracking-widest text-content-muted font-bold">
+            © {currentYear} Hackhim
           </p>
-
-          <div className="flex gap-6 text-[11px] uppercase tracking-widest font-bold text-content-muted">
-            <Link href="" className="hover:text-brand transition-colors">
-              Privacy Policy
-            </Link>
-            <Link href="" className="hover:text-brand transition-colors">
-              Cookies Policy
-            </Link>
-          </div>
         </div>
       </div>
     </footer>
